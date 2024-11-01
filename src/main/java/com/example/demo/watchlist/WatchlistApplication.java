787777d58f560e8e30cca9efae9daf5ce855c693
Priyasha Gupta <57822712+Priyasha2909@users.noreply.github.com>
2024-11-01 19:43:36 +0530

    Update and rename WatchlistApplication.java to WatchlistApplication.java

--- src/main/java/com/example/demo/watchlist/WatchlistApplication.java
+++ src/main/java/com/example/demo/watchlist/WatchlistApplication.java
@@ -1,4 +1,4 @@
-package com.example.ashutosh.watchlist;
+package com.example.demo.watchlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
